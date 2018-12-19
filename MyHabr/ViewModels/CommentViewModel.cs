eb006cf3c436186d0f2d126bd5512f60e5daad49
Tubu Ibragimova <tubuazra@gmail.com>
2018-12-19 13:29:28 +0400

    Comment ErrorMessage added

--- MyHabr/ViewModels/CommentViewModel.cs
+++ MyHabr/ViewModels/CommentViewModel.cs
@@ -10,6 +10,7 @@ namespace MyHabr.ViewModels
 
         [Display(Name = "Text")]
         [Required(ErrorMessage = "Comment is required")]
+        [StringLength(500, MinimumLength = 5, ErrorMessage = "Text must be between 5 and 500 characters")]
         public string Text { get; set; }
     }
 }
